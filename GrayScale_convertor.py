fde0c1c3cde0edb806118247dcad290564faa48c
Gowri_shankar_02 <48479513+Gowri-Shankar@users.noreply.github.com>
2019-03-12 18:46:58 +0530

    Create GrayScale_convertor.py

---
+++ GrayScale_convertor.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import sys
+import os
+file="D:\colour_to_grey\my\convert" #Folder Path with need to be converted
+items=os.listdir(file)
+i=0
+result_path="D:\\colour_to_grey\\my\\result" #Folder Path to save result image
+for item in items:
+    path=file+"\\" +item
+    print (path)
+    image = cv2.imread(path)
+    gray_image= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # converting image to gray
+    new_path=result_path+"\\"+"image"+str(i)+".jpg"
+    print (new_path)
+    cv2.imwrite(new_path,gray_image)   # saves gray image to disk
+    i=i+1
+cv2.imshow('color_image',image)
+cv2.imshow('gray_image',gray_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
